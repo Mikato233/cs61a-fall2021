989d99a41cbf182c3471231a15143c2a7de5bfa1
Mikato <1902432549@qq.com>
2022-01-20 15:01:04 +0800

    01

--- lab00/lab00.py
+++ lab00/lab00.py
@@ -1,8 +1,8 @@
-def twenty_eighteen():
-    """Come up with the most creative expression that evaluates to 2018,
+def twenty_twenty_one():
+    """Come up with the most creative expression that evaluates to 2021,
     using only numbers and the +, *, and - operators.
 
-    >>> twenty_eighteen()
-    2018
+    >>> twenty_twenty_one()
+    2021
     """
-    return 2018 
+    return 2000 + 21
