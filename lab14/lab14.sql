fe4281ddfdadd9ebc5ca51eef4d8377711c472f5
Mikato <1902432549@qq.com>
2022-03-10 18:17:20 +0800

    lab14!

---
+++ lab14/lab14.sql
@@ -0,0 +1,22 @@
+create table pizzas as
+  select "Pizzahhh" as name, 12 as open, 15 as close union
+  select "La Val's"        , 11        , 22          union
+  select "Sliver"          , 11        , 20          union
+  select "Cheeseboard"     , 16        , 23          union
+  select "Emilia's"        , 13        , 18;
+
+create table meals as
+  select "breakfast" as meal, 11 as time union
+  select "lunch"            , 13         union
+  select "dinner"           , 19         union
+  select "snack"            , 22;
+
+
+-- Pizza places that open before 1pm in alphabetical order
+create table opening as
+SELECT name FROM pizzas WHERE open < 13;
+
+
+-- Two meals at the same place
+create table double as
+SELECT early.meal, late.meal, name FROM pizzas, meals AS early, meals AS late WHERE late.time - early.time > 6 AND open <= early.time AND close >= late.time;
